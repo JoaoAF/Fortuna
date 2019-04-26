e51530d8a5bb9d9d1bc4ab1c315ee53a17f6ee05
João Paulo de Araújo Ferreira <joao.ferreira@br-ifce-133.local>
2019-04-26 14:59:41 -0300

    Subindo os arquivos

---
+++ fortuna/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  fortuna
+//
+//  Created by João Paulo de Araújo Ferreira on 26/04/19.
+//  Copyright © 2019 João Paulo de Araújo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
